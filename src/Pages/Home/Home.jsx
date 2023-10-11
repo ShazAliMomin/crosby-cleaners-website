b9b947e5194643eeee82568f97d9420c7c9f6095
ShazAliMomin <momin.shazali01@gmail.com>
2023-10-11 15:02:04 -0500

    Adding hero section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,19 @@
 import React from "react";
+import "./Home.css";
+import IntroImg from "../../images/Laundry-clipart-image.jpeg";
 
 function Home() {
   return (
-    <section>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <section className="home">
+      <div className="intro">
+        <div className="intro-left">
+          <h1>
+            <span>Same Day</span> Dry Cleaning and Laundry Services
+          </h1>
+          <button>Learn More</button>
+        </div>
+        <img src={IntroImg} alt="Clip art of dry cleaners"></img>
+      </div>
     </section>
   );
 }
