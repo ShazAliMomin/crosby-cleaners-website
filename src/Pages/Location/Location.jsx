6cd2f3d9c6825780924b9aa8bd79d43cbb6d818d
ShazAliMomin <momin.shazali01@gmail.com>
2024-01-23 16:43:23 -0600

    Added Location Page

--- src/Pages/Location/Location.jsx
+++ src/Pages/Location/Location.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Location.css";
 
+import GoogleMapImg from "../../images/CrosbyLocation.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faClock,
@@ -8,58 +9,60 @@ import {
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 
-const location = {
-  address: "14045 FM 2100 Suite 140, Crosby, TX",
-  lat: 29.893198631619864,
-  lng: -95.06347417044317,
-};
-
 function Location() {
   return (
     <section className="location">
       <h1>Our Location</h1>
-      <div className="location-info">
-        <div className="address">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <p>14045 FM 2100 Suite 140, Crosby, TX 77532</p>
+      <div className="location-container">
+        <div className="location-info">
+          <div className="address">
+            <FontAwesomeIcon className="contact-icon" icon={faLocationDot} />
+            <p>14045 FM 2100 Suite 140, Crosby, TX 77532</p>
+          </div>
+
+          <div className="phone">
+            <FontAwesomeIcon className="contact-icon" icon={faPhone} />
+            <p>(281) 328-1519</p>
+          </div>
+
+          <div className="open-times">
+            <div>
+              <FontAwesomeIcon className="contact-icon" icon={faClock} />
+            </div>
+            <div className="times">
+              <p>
+                Sunday: <span className="time-details">Closed</span>
+              </p>
+              <p>
+                Monday: <span className="time-details">6:30am - 7:00pm</span>
+              </p>
+              <p>
+                Tuesday: <span className="time-details">6:30am - 7:00pm</span>{" "}
+              </p>
+              <p>
+                Wednesday: <span className="time-details">6:30am - 7:00pm</span>
+              </p>
+              <p>
+                Thursday: <span className="time-details">6:30am - 7:00pm</span>
+              </p>
+              <p>
+                Friday: <span className="time-details">6:30am - 7:00pm</span>
+              </p>
+              <p>
+                Saturday: <span className="time-details">7:00am - 4:00pm</span>
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div className="phone">
-          <FontAwesomeIcon icon={faPhone} />
-          <p>(281) 328-1519</p>
-        </div>
-
-        <div className="open-times">
-          <div>
-            <FontAwesomeIcon icon={faClock} />
-          </div>
-          <div className="times">
-            <p>
-              Sunday: <span className="time-details">Closed</span>
-            </p>
-            <p>
-              Monday: <span className="time-details">6:30am - 7:00pm</span>
-            </p>
-            <p>
-              Tuesday: <span className="time-details">6:30am - 7:00pm</span>{" "}
-            </p>
-            <p>
-              Wednesday: <span className="time-details">6:30am - 7:00pm</span>
-            </p>
-            <p>
-              Thursday: <span className="time-details">6:30am - 7:00pm</span>
-            </p>
-            <p>
-              Friday: <span className="time-details">6:30am - 7:00pm</span>
-            </p>
-            <p>
-              Saturday: <span className="time-details">7:00am - 4:00pm</span>
-            </p>
-          </div>
-        </div>
+        {/* <div className="map"> */}
+        <img
+          className="google-img"
+          src={GoogleMapImg}
+          alt="Crosby Location on Google Maps"
+        />
+        {/* </div> */}
       </div>
-
-      <div className="map"></div>
     </section>
   );
 }
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,27 +1,17 @@
 import React from "react";
+import "./Services.css";
 
 function Services() {
   return (
-    <section>
-      <h1>Services</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <section className="services">
+      <h1>Our Services</h1>
+
+      <div className="dry-laundry-container">
+        <div className="dry-laundry-info">
+          <p>What we offer</p>
+        </div>
+        <div className="dry-laundry-img"></div>
+      </div>
     </section>
   );
 }
