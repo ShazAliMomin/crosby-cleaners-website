c31113c5617cbd07a19e29679e0d91f079669f13
ShazAliMomin <momin.shazali01@gmail.com>
2024-02-01 16:19:18 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b254d535c5e94c787091a79a7c1c8d9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024794da441c211766627799f8ab5231",
+    "url": "/crosby-cleaners-website/index.html"
+  },
+  {
+    "revision": "446871e92f5dc6e9a068",
+    "url": "/crosby-cleaners-website/static/css/main.0b03923e.chunk.css"
+  },
+  {
+    "revision": "c183786a37511027d115",
+    "url": "/crosby-cleaners-website/static/js/2.0f8d5856.chunk.js"
+  },
+  {
+    "revision": "446871e92f5dc6e9a068",
+    "url": "/crosby-cleaners-website/static/js/main.e795e713.chunk.js"
+  },
+  {
+    "revision": "4141c02543e3291fdbe2",
+    "url": "/crosby-cleaners-website/static/js/runtime~main.128e78bc.js"
+  },
+  {
+    "revision": "876d6c1c8273ca79514b7d3b9250a625",
+    "url": "/crosby-cleaners-website/static/media/Alteration.876d6c1c.png"
+  },
+  {
+    "revision": "7034facbb24c954988fb513d66baea79",
+    "url": "/crosby-cleaners-website/static/media/CrosbyCoupon.7034facb.png"
+  },
+  {
+    "revision": "afb9665169defffb1c42e69900d0acca",
+    "url": "/crosby-cleaners-website/static/media/HangedClothes.afb96651.png"
+  },
+  {
+    "revision": "8aa7f538c34f067c4275086e2bf082d4",
+    "url": "/crosby-cleaners-website/static/media/Laundry-clipart.8aa7f538.png"
+  },
+  {
+    "revision": "54766b5d8b28be987d6bde944ded2228",
+    "url": "/crosby-cleaners-website/static/media/Laundry.54766b5d.png"
+  },
+  {
+    "revision": "b97011677c1cacf7f9101d2daefdbcbf",
+    "url": "/crosby-cleaners-website/static/media/Logo-sm-2.b9701167.png"
+  },
+  {
+    "revision": "76d74d9729fafb0c58bec9a104487803",
+    "url": "/crosby-cleaners-website/static/media/Logo-sm-3.76d74d97.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crosby-cleaners-website/precache-manifest.b254d535c5e94c787091a79a7c1c8d9d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crosby-cleaners-website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
